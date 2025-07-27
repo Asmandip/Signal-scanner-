e2ff5275843193e8c636be7d25721702a5d9ee55
Asmandip <asmandip@gmail.com>
2025-07-27 19:27:28 +0530

    utils/telegram.py

---
+++ utils/telegram.py
@@ -0,0 +1,18 @@
+import os
+import aiohttp
+import asyncio
+
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
+
+async def send_telegram_message(text):
+    url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
+    payload = {"chat_id": CHAT_ID, "text": text}
+
+    async with aiohttp.ClientSession() as session:
+        try:
+            async with session.post(url, json=payload) as resp:
+                if resp.status != 200:
+                    print(f"Telegram Error: {await resp.text()}")
+        except Exception as e:
+            print(f"Telegram Send Failed: {e}")
